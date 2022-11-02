7fa7b39ed0bcd46cb48f8134eefb905b54b02d26
randomuseraccnt <65951729+randomuseraccnt@users.noreply.github.com>
2022-11-02 15:25:01 +0000

    Create Trade.cs

---
+++ Trade.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_Practice
+{
+    public class Trade
+    {
+        public string Asset { get; set; }
+        public int Amount { get; set; }
+
+        public string Direction { get; set; }
+
+        public int Price { get; set; }
+
+        // Methods
+   
+    }
+}
